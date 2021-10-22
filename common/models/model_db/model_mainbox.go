f8e98da47c9f53ee90f2d25f098aa9db404154c0
jjoy <42736097+jjoykkm@users.noreply.github.com>
2021-10-23 02:51:59 +0700

    add serial_no

--- common/models/model_db/model_mainbox.go
+++ common/models/model_db/model_mainbox.go
@@ -11,16 +11,17 @@ import (
 //-------------------------------------------------------------------------------//
 //model mainbox
 type Mainbox struct {
-	MainboxId      	uuid.UUID	 `json:"mainbox_id"`
-	MainboxName     string		 `json:"mainbox_name"`
-	MainboxModel    string		 `json:"mainbox_model"`
-	MainboxLots     string		 `json:"mainbox_lots"`
-	StartWarranty	time.Time	 `json:"start_warranty"`
-	EndWarranty		time.Time	 `json:"end_warranty"`
-	CreateDate		time.Time	 `json:"create_date"`
-	ChangeDate	    time.Time	 `json:"change_date"`
-	StatusId		uuid.UUID	 `json:"status_id"`
-	FarmId			uuid.UUID	 `json:"farm_id"`
+	MainboxId      		uuid.UUID	 `json:"mainbox_id"`
+	MainboxName     	string		 `json:"mainbox_name"`
+	MainboxModel    	string		 `json:"mainbox_model"`
+	MainboxLots     	string		 `json:"mainbox_lots"`
+	StartWarranty		time.Time	 `json:"start_warranty"`
+	EndWarranty			time.Time	 `json:"end_warranty"`
+	CreateDate			time.Time	 `json:"create_date"`
+	ChangeDate	    	time.Time	 `json:"change_date"`
+	StatusId			uuid.UUID	 `json:"status_id"`
+	FarmId				uuid.UUID	 `json:"farm_id"`
+	MainboxSerialNo		uuid.UUID	 `json:"serial_no"`
 }
 // New instance mainbox
 func (u *Mainbox) New() *Mainbox {
@@ -35,6 +36,7 @@ func (u *Mainbox) New() *Mainbox {
 		ChangeDate:			u.ChangeDate ,
 		StatusId:			u.StatusId ,
 		FarmId:				u.FarmId ,
+		MainboxSerialNo:	u.MainboxSerialNo ,
 	}
 }
 
