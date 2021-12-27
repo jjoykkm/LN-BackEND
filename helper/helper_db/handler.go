1ac79b67a8518485928e0de9ee1958d9a8ac39b0
jjoy <42736097+jjoykkm@users.noreply.github.com>
2021-12-27 21:59:11 +0700

    delete helper

--- helper/helper_db/handler.go
+++
@@ -1,9 +0,0 @@
-package helper_db
-
-type Handler struct {
-	service Servicer
-}
-
-func NewHandler(service Servicer) *Handler {
-	return &Handler{service: service}
-}
--- helper/helper_gorm/handler.go
+++
@@ -1,9 +0,0 @@
-package helper_gorm
-
-type Handler struct {
-	service Servicer
-}
-
-func NewHandler(service Servicer) *Handler {
-	return &Handler{service: service}
-}
